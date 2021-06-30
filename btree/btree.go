67509aa3773f84abcbc8cb1f5dc8efb3f8c399bb
jamillepp <jamillepp@github.com>
2021-06-30 13:48:44 -0300

    Changed structue

--- btree/btree.go
+++ btree/btree.go
@@ -2,16 +2,20 @@ package btree
 
 import (
 	"fmt"
-	"sort"
 )
 
 type btree struct {
-	ord   int
-	typ   int // -1: root, 0: inter, 1: leaf
-	m     int
-	page  []int
-	child []*btree
-	anc   *btree
+	ord  int
+	typ  int // -1: root, 0: inter, 1: leaf
+	m    int
+	page []*key
+	anc  *btree
+}
+
+type key struct {
+	value int
+	c0    *btree
+	c1    *btree
 }
 
 func Initbtree(o int) btree {
@@ -23,70 +27,103 @@ func Initbtree(o int) btree {
 }
 
 func (b *btree) Insert(k int) {
-	if b.typ == 1 {
+
+	newkey := &key{
+		value: k,
+	}
+
+	if b.typ == 1 { // Se for folha
 		if b.m < b.ord*2 {
-			b.page = append(b.page, k)
+			b.page = append(b.page, newkey)
 			b.m += 1
-			sort.Ints(b.page)
+			sortpage(b.page)
+			return
 		} else if b.m == b.ord*2 {
-			b.page = append(b.page, k)
+			b.page = append(b.page, newkey)
+			sortpage(b.page)
 			b.split()
+			return
 		}
 	} else {
-		if k < b.page[0] {
-			b.child[0].Insert(k)
+		if newkey.value < b.page[0].value { // Entra na p0
+			b.page[0].c0.Insert(k)
+		} else if newkey.value > b.page[len(b.page)-1].value { // Entra na pm
+			b.page[len(b.page)-1].c1.Insert(k)
+		} else {
+			fmt.Println("pi")
 		}
-		if k > b.page[len(b.page)-1] {
-			b.child[len(b.child)-1].Insert(k)
+	}
+}
+
+func sortpage(p []*key) {
+	for i := 0; i < len(p); i++ {
+		if i != len(p)-1 && p[i].value > p[i+1].value {
+			t := p[i]
+			p[i] = p[i+1]
+			p[i+1] = t
 		}
-		// for i := 0; i < len(b.page); i++ {
-		// 	if i == len(b.page)-1 {
-		// 		if
-		// 	}
-		// }
 	}
 }
 
 func (b *btree) split() {
 	if b.anc == nil {
-		cp := make([]int, len(b.page))
+		cp := make([]*key, len(b.page))
 		copy(cp, b.page)
-		pivotid := int(len(cp) / 2)
-		pivot := cp[pivotid]
-		f := cp[:pivotid]
-		s := cp[pivotid+1:]
-		b.page = []int{pivot}
+		indpivot := int(len(cp) / 2)
+		pivot := cp[indpivot]
+		ch0 := &btree{
+			ord:  b.ord,
+			typ:  1,
+			m:    b.ord,
+			page: cp[:indpivot],
+			anc:  b,
+		}
+		ch1 := &btree{
+			ord:  b.ord,
+			typ:  1,
+			m:    b.ord,
+			page: cp[indpivot+1:],
+			anc:  b,
+		}
 		b.typ = -1
-		b.m = 1
-		c0 := btree{
-			ord:  b.ord,
-			typ:  1,
-			m:    2,
-			page: f,
-			anc:  b,
+		b.page = []*key{
+			{
+				value: pivot.value,
+				c0:    ch0,
+				c1:    ch1,
+			},
 		}
-		c1 := btree{
-			ord:  b.ord,
-			typ:  1,
-			m:    2,
-			page: s,
-			anc:  b,
-		}
-		b.child = append(b.child, &c0)
-		b.child = append(b.child, &c1)
 	} else {
-		pivotid := int(len(b.page) / 2)
-		pivot := b.page[pivotid]
-		b.anc.page = append(b.anc.page, pivot)
-		b.page = append(b.page[:pivotid], b.page[pivotid+1:]...)
+		fmt.Println("...")
 	}
 }
 
 func (b *btree) Print() {
-	fmt.Println(b.typ, b.page)
-	for _, p := range b.child {
-		if p != nil {
-			p.Print()
+	b.print([]*btree{})
+}
+
+func (b *btree) print(queue []*btree) {
+	fmt.Printf("\n[")
+	for i, k := range b.page {
+		fmt.Printf("%v", k.value)
+		if k.c0 != nil {
+			queue = append(queue, k.c0)
+		}
+		if k.c1 != nil {
+			queue = append(queue, k.c1)
+		}
+		if i != len(b.page)-1 {
+			fmt.Printf(" ")
 		}
 	}
+	fmt.Printf("]")
+	for i, v := range queue {
+		if len(queue) == 1 {
+			queue = []*btree{}
+		} else {
+			queue = append(queue[:i], queue[i+1:]...)
+		}
+		v.print(queue)
+	}
+	fmt.Println("")
 }
